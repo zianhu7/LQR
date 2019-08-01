8cbd12b1add208d10417e4f6d2caac6c9cb76a3b
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-08-01 16:18:18 -0700

    Convert argparse bools to ints, bools dont work

--- utils/parsers.py
+++ utils/parsers.py
@@ -16,25 +16,25 @@ def env_args(parser):
     parser.add_argument("--eigv_high", type=float, default=2.0, help="Maximum absolute value of eigenvalue A can have. "
                                                                      "If A has an eigenvalue above this, we "
                                                                      "sample a new A.")
-    parser.add_argument("--elem_sample", type=bool, default=1, help="If true, the elements of A and B are sampled "
+    parser.add_argument("--elem_sample", type=int, default=1, help="If true, the elements of A and B are sampled "
                                                                     "randomly")
-    parser.add_argument("--eval_matrix", type=bool, default=0, help="If this is true, the A and B matrices are fixed to"
+    parser.add_argument("--eval_matrix", type=int, default=0, help="If this is true, the A and B matrices are fixed to"
                                                                     "the values from 'On the sample complexity of the "
                                                                     "linear quadratic regulator")
-    parser.add_argument("--full_ls", type=bool, default=1, help="If true, we use all the samples from the rollouts for"
+    parser.add_argument("--full_ls", type=int, default=1, help="If true, we use all the samples from the rollouts for"
                                                                 "Least Squares. If not, we just use the last input"
                                                                 "output pair")
     parser.add_argument("--dim", type=int, default=3, help="Dim of A and B matrices")
     # TODO(@evinitsky) remove this param, it seems ultra risky to have both it and eval_matrix.
-    parser.add_argument("--eval_mode", type=bool, default=0, help="Disable a few peculiarities of training for "
+    parser.add_argument("--eval_mode", type=int, default=0, help="Disable a few peculiarities of training for "
                                                                   "evaluation time. If evaluating, SET TO TRUE")
-    parser.add_argument("--analytic_optimal_cost", type=bool, default=1, help="If true, compute the optimal cost of "
+    parser.add_argument("--analytic_optimal_cost", type=int, default=1, help="If true, compute the optimal cost of "
                                                                               "the synthesized system analytically")
-    parser.add_argument("--gaussian_actions", type=bool, default=0, help="If true, the actions are sampled from a "
+    parser.add_argument("--gaussian_actions", type=int, default=0, help="If true, the actions are sampled from a "
                                                                          "Gaussian with mean 0 and identity cov matrix "
                                                                          "Otherwise, they are taken from the neural"
                                                                          "network")
-    parser.add_argument("--rand_num_exp", type=bool, default=1, help="If true, the max number of trials is sampled"
+    parser.add_argument("--rand_num_exp", type=int, default=1, help="If true, the max number of trials is sampled"
                                                                      "uniformly from 2 * dim to (horizon / exp_length "
                                                                      "Otherwise, the max num is horizon / exp length")
 
@@ -48,13 +48,13 @@ def GenLQRParserRLlib():
     parser.add_argument("exp_title", type=str, help="Name of experiment. The results will be saved to a folder"
                                                     "with this name")
     parser.add_argument("--num_cpus", type=int, default=2, help="Number of CPUs to use in the trial")
-    parser.add_argument("--multi_node", type=bool, default=0, help="If true, run RLlib in cluster mode")
-    parser.add_argument("--use_s3", type=bool, default=0, help="Upload results to s3")
+    parser.add_argument("--multi_node", type=int, default=0, help="If true, run RLlib in cluster mode")
+    parser.add_argument("--use_s3", type=int, default=0, help="Upload results to s3")
     parser.add_argument("--checkpoint_freq", type=int, default=1, help="How many iterations are required for a model "
                                                                        "checkpoint.")
     parser.add_argument("--num_iters", type=int, default=1, help="Total number of gradient steps")
     parser.add_argument("--num_samples", type=int, default=1, help="How many times to repeat each experiment")
-    parser.add_argument("--grid_search", type=bool, default=0, help="Do a tune grid search if true")
+    parser.add_argument("--grid_search", type=int, default=0, help="Do a tune grid search if true")
     parser.add_argument("--train_batch_size", type=int, default=30000, help="How many steps in a gradient batch")
     return parser
 
@@ -63,7 +63,7 @@ def GenLQRParserBaseline():
     parser = argparse.ArgumentParser()
     env_args(parser)
     # ================================================================================================
-    #                    RLLIB PARAMS
+    #                    PARAMS
     # ================================================================================================
     parser.add_argument("exp_title", type=str, help="Name of experiment. The results will be saved to a folder"
                                                     "with this name")
@@ -74,7 +74,7 @@ def GenLQRParserBaseline():
                                                               "the path is pretty hardcoded")
     parser.add_argument('--checkpoint_freq', type=int, default=1, help="How often to check if the model has improved "
                                                                        "and consequently save it")
-    parser.add_argument('--callback', type=bool, default=1, help="Whether to save the model using a callback or just"
+    parser.add_argument('--callback', type=int, default=1, help="Whether to save the model using a callback or just"
                                                                  "at the end of the training")
     return parser
 
@@ -85,10 +85,10 @@ def RolloutParser():
     env_args(parser)
     parser.add_argument("--steps", type=int, default=10000, help="Number of steps to roll out.")
     parser.add_argument("--out", help="Output filename.")
-    parser.add_argument("--append", type=bool, default=1, help="If absent, files are written instead of appended")
-    parser.add_argument("--eigv_gen", type=bool, default=False,
+    parser.add_argument("--append", type=int, default=1, help="If absent, files are written instead of appended")
+    parser.add_argument("--eigv_gen", type=int, default=False,
                         help="Eigenvalue generalization tests for eigenvalues of A. Sample matrices randomly and "
                              "see how we perform relative to the top eigenvalue")
-    parser.add_argument("--opnorm_error", type=bool, default=False,
+    parser.add_argument("--opnorm_error", type=int, default=False,
                         help="Operator norm error of (A-A_est)")
     return parser
