fa730e6f6ded6ad99486aebe48a6a98cd5c5abe8
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-08-05 19:52:03 -0700

    Commit for 8/5 code

--- envs/GenLQREnv.py
+++ envs/GenLQREnv.py
@@ -173,25 +173,6 @@ class GenLQREnv(gym.Env):
     #                                   UTILITY FUNCTIONS
     ###############################################################################################
 
-    # Generate random orthornormal matrix of given dim
-    def rvs(self, dim):
-        random_state = np.random
-        H = np.eye(dim)
-        D = np.ones((dim,))
-        for n in range(1, dim):
-            x = random_state.normal(size=(dim - n + 1,))
-            D[n - 1] = np.sign(x[0])
-            x[0] -= D[n - 1] * np.sqrt((x * x).sum())  # Householder transformation
-            Hx = (np.eye(dim - n + 1) - 2. * np.outer(x, x) / (x * x).sum())
-            mat = np.eye(dim)
-            mat[n - 1:, n - 1:] = Hx
-            H = np.dot(H, mat)
-            # Fix the last sign such that the determinant is 1
-        D[-1] = (-1) ** (1 - (dim % 2)) * D.prod()
-        # Equivalent to np.dot(np.diag(D), H) but faster, apparently
-        H = (D * H.T).T
-        return H
-
     def ls_estimate(self):
         """Compute an LS estimate of the A and B matrices"""
 
